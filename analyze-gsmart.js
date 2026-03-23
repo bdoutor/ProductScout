f29bbca1347ec7abd7395684015c0ee6d78db4a6
Bruno Doutor <bdoutor@gmail.com>
2026-03-23 21:52:59 +0000

    V 5.1
    Pequenos ajustes de FE

--- analyze-gsmart.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const cheerio = require('cheerio');
-const html = fs.readFileSync('Backend/gsmart-results.html','utf8');
-const $ = cheerio.load(html);
-console.log('tr count', $('tr').length);
-console.log('table classes', $('table').map((i,el)=>$(el).attr('class')).get().filter(Boolean));
-$('tr').slice(0,5).each((i,el)=>console.log('row', i, $(el).text().trim().slice(0,200)));
